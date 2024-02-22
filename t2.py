41f3c92fe1b9533358c2939bcfe531a5c30e8b3b
Alexey Golovnia <alexey_golovnia@Alexeym1.local>
2024-02-22 20:52:31 +0100

    t7

---
+++ t2.py
@@ -0,0 +1,78 @@
+
+from multiprocessing.shared_memory import SharedMemory
+import numpy as np
+import easyocr
+reader = easyocr.Reader(['en'])
+import time
+
+
+def my_process(tmp, width, height, len_contours, contours_size):
+    frame_memory = SharedMemory('FrameMemory')
+    contours_memory = SharedMemory('ContoursMemory')
+
+    while True:
+        if contours_size.value != 0:
+            frame_data = np.ndarray((height,width,3), dtype=np.uint8, buffer=frame_memory.buf)
+            contours = reader.detect(frame_data, min_size=5, text_threshold=0.7)[0][0]
+
+            if len(contours) > 0:
+                len_contours.value = len(contours)
+                contours_data = np.ndarray((len_contours.value, 4), dtype=np.int32, buffer=contours_memory.buf)
+                contours_data[:, :] = contours
+
+            tmp.value = 1
+
+# def my_process(tmp, width, height, len_contours, contours_size):
+ #   frame_memory = SharedMemory('FrameMemory')
+ #   contours_memory = SharedMemory('ContoursMemory')
+
+ #   while True:
+ #       if contours_size.value != 0:
+ #           frame_data = np.ndarray((height,width,3), dtype=np.uint8, buffer=frame_memory.buf)
+ #           contours = reader.detect(frame_data, min_size=5, text_threshold=0.7)[0][0]
+            # len_contours.value = len(contours) # перенести ниже
+
+            # if len_contours.value > 0:
+ #           if len(contours) > 0:
+ #               len_contours.value = len(contours)
+ #               contours_data = np.ndarray((len_contours.value, 4), dtype=np.int32, buffer=contours_memory.buf)
+ #               contours_data[:, :] = contours
+            # else:
+                # contours_data = np.ndarray((1, 4), dtype=np.int32, buffer=contours_memory.buf)
+                # contours_data[:, :] = [[0,0,0,0]]
+                # len_contours.value = 1
+
+#            tmp.value = 1
+
+
+# def my_process(tmp, width, height, len_contours, contours_size):
+#     frame_memory = SharedMemory('FrameMemory')
+#     contours_memory = SharedMemory('ContoursMemory')
+#
+#     while True:
+#         if contours_size.value != 0:
+#             frame_data = np.ndarray((height,width,3), dtype=np.uint8, buffer=frame_memory.buf)
+#             contours = reader.detect(frame_data, min_size=5, text_threshold=0.7)[0][0]
+#             len_contours.value = len(contours)
+#             contours_data = np.ndarray((len_contours.value, 4), dtype=np.int32, buffer=contours_memory.buf)
+#             contours_data[:, :] = contours
+#             tmp.value = 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
